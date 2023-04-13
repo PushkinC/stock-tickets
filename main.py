69fdf7ffca4a22bb1f4740022a6e5e39636fa1f6
PushkinC <zolotarev_d_e@mail.ru>
2023-04-13 07:00:02 +0300

    9

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, app, url_for
+import string
+import io
 import csv
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -6,6 +7,10 @@ import atexit
 import os
 import create_plots
 import warnings
+import base64
+from json import dumps
+from PIL import Image
+from flask import Flask, render_template, app, url_for, request, redirect, send_file
 
 
 warnings.filterwarnings("ignore")
@@ -16,9 +21,9 @@ app = Flask(__name__)
 PORT = 8080
 HOST = '127.0.0.1'
 
-@app.route('/')
 @app.route('/index')
 def index(): # Страница с графиками
+    global cur_user
     tablo = []
     buy = []
     sale = []
@@ -55,10 +60,7 @@ def index(): # Страница с графиками
         print('Я в открытии данных для таблиц!!!!!!')
 
 
-    return render_template('index.html', tablo=tablo, buy=buy, sale=sale)
-
-
-
+    return render_template('index.html', tablo=tablo, buy=buy, sale=sale, user=cur_user)
 
 
 def create_HHTP_tablet(name): # Функция подготавливающая данные для HTML графика
@@ -85,8 +87,6 @@ def ticket(name): # Главная страница
     return render_template('ticket.html', name=name, data=data, diff=diff)
 
 
-
-
 def goodbye():
     path = 'static/Pictures/temp'
 
@@ -96,22 +96,134 @@ def goodbye():
             os.remove(path + '/' + i) # Удаление графиков
 
 
+@app.route('/register', methods=['post', 'get'])
+def register():
+    message = ''
+    if request.method == 'POST':
+        name = request.form.get('name')
+        mail = request.form.get('mail')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        message = valid_pass_and_mail(name, mail, password1, password2)
+        if message == '':
+            addUser(name, mail, password1)
+            return redirect('/login')
+
+    return render_template('register.html', message=message)
+
+@app.route('/login', methods=['post', 'get'])
+def login():
+    global cur_user
+    message = ''
+    if request.method == 'POST':
+        mail = request.form.get('mail')
+        password = request.form.get('password')
+        message = check_pass_and_mail(mail, password)
+        if message == '':
+            for i in users:
+                if i[1] == mail:
+                    cur_user = i[0]
+            return redirect('/index')
+
+    return render_template('login.html', message=message)
+
+
+def valid_pass_and_mail(name, mail, password, password2) -> str:
+    message = ''
+    if name == '':
+        return 'Имя не указано'
+    if mail == '':
+        return 'Почта не указана'
+    if password == '':
+        return 'Пароль не указан'
+    if password2 == '':
+        return 'Пароль не указан'
+
+    if '@' in mail and '.'  in mail:
+        if not(1 < mail.index('@') < mail.index('.') - 1 and mail.index('.') + 1 < len(mail)):
+            message = 'Неверная почта'
+    else:
+        message = 'Неверная почта'
+
+    if password == password2:
+        if not any([x in password for x in string.digits]) or not any(
+                [x in password for x in string.ascii_letters]) or len(password) < 5:
+            if message != '':
+                message = 'Неверные почта и пароль'
+            else:
+                message = 'Неверный пароль'
+    else:
+        message = 'Пароли не совпадают'
+
+    return message
+
+def check_pass_and_mail(mail, password) -> str:
+    for i in users:
+        if mail == i[1]:
+            if password ==  i[2]:
+                return ''
+    return 'Неверные почта или пароль'
+
+
+
+def openUsers() -> list:
+    with open('static/csv/users.csv', 'r', encoding='UTF-8') as f:
+        reader = csv.reader(f)
+        users = []
+        for row in reader:
+            users.append(row[0].split(';'))
+    return users
+
+def addUser(name, mail, password):
+    global users
+    with open("static/csv/users.csv", mode="w", encoding='utf-8') as f:
+        file_writer = csv.writer(f, delimiter=";", lineterminator="\n")
+        for i in users:
+            file_writer.writerow(i)
+        file_writer.writerow([name, mail, password])
+    users = openUsers()
+
+
+@app.route('/api/ticker/<name>', methods=['get'])
+def rest_get_ticker(name):
+    name = name.upper()
+    p = pd.read_csv('static/csv/d100.csv')
+    if name in p.keys():
+        data, diff = create_HHTP_tablet(name)  # Подготовка данных для HTTP графика
+        create_plots.save_plot_by_name(name)  # Создание графика цен
+        create_plots.save_predict_by_name(name)  # Создание графика предсказанных цен
+
+        img1 = getImageBytes(f'static/Pictures/temp/{name}.png')
+        img2 = getImageBytes(f'static/Pictures/temp/{name}1.png')
+
+        ans = {
+            'name': name,
+            'data': data,
+            'diff': diff,
+            'url-plot1': img1,
+            'url-plot2': img2
+        }
+        return dumps(ans)
+    else:
+        return dumps({'error': 'Name not found'})
+
+
+def getImageBytes(filePath):
+    pil_img = Image.open(filePath, mode='r')  # reads the PIL image
+    byte_arr = io.BytesIO()
+    pil_img.save(byte_arr, format='PNG')  # convert the PIL image to byte array
+    encoded_img = base64.encodebytes(byte_arr.getvalue()).decode('ascii')  # encode as base64
+    return encoded_img
+
+
 atexit.register(goodbye) # Перехват выхода из приложения
 
 
 
-
 if __name__ == '__main__':
-    import parser
-    import recommendation
-    print('asdasdasd')
-    app.run(port=8080, host='0.0.0.0')
-
-
-# docker tag local-image:tagname new-repo:tagname
-# docker push new-repo:tagname
-
-
-
-
-
+    # import parser
+    # import recommendation
+    users = openUsers()
+    cur_user = 'Аноним'
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
